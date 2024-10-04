13bdf5535ac5a224d51cde3eb93b52a4e787ad25
adolfo segura <adolfosegura@ccv.church>
2024-10-04 14:40:26 -0700

    New files

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./home.css";
+
+const Home = ()=> {
+    return (
+        <div className='container-fluid home'>
+            <div className='container home-content'>
+                <p>test teste sestestsersdgsdgf sdgsdgs fsdgsd sdgsdfg sdfgsdfgsdfgsd fgsdfgsdgsdfg sdf.</p>
+                <span className='view-details-home-button'>View Details</span>
+            </div>
+        </div>
+    );
+};
+
+export default Home
