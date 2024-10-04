ec8eb79cc2e32f37b980d8bdfaf0775da97427f7
adolfo segura <adolfosegura@ccv.church>
2024-10-04 15:01:16 -0700

    Progress

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import "./home.css";
+import Typewriter from 'typewriter-effect';
+
 
 const Home = ()=> {
     return (
         <div className='container-fluid home'>
             <div className='container home-content'>
+
+                <h1>
+                <Typewriter 
+                    options={{
+                        strings: [
+                            'Web Development', 
+                            'Mobile App Development',
+                            'UI/UX Design'],
+                        autoStart: true,
+                        loop: true,
+                        delay: 5
+                    }}
+                />
+
+                </h1>
+
                 <p>test teste sestestsersdgsdgf sdgsdgs fsdgsd sdgsdfg sdfgsdfgsdfgsd fgsdfgsdgsdfg sdf.</p>
                 <span className='view-details-home-button'>View Details</span>
             </div>
